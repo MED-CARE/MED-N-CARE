d9818171ad2d7636e03a1ff7656137cbd39df4c2
SouravCodes1080 <107361153+souravcodes1080@users.noreply.github.com>
2024-06-28 23:49:52 +0530

    created index page

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+import { connectdb } from "./db/index.js";
+import { userRouter } from "./routes/user.route.js";
+import { hospitalRouter } from "./routes/hospital.route.js";
+import { doctorRouter } from "./routes/doctor.route.js";
+import { pharmacyRouter } from "./routes/pharmacy.route.js";
+import { appointmentRouter } from "./routes/appointment.route.js";
+import { orderRouter } from "./routes/order.route.js";
+
+//middlewares
+const app = express();
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json());
+
+connectdb();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//api endpoints
+app.use("/api/user", userRouter);
+app.use("/api/hospital", hospitalRouter)
+app.use("/uploads", express.static("uploads"));
+app.use("/api/doctor", doctorRouter)
+app.use("/api/pharmacy", pharmacyRouter)
+app.use("/api/appointment", appointmentRouter)
+app.use("/api/order", orderRouter)
+
+app.listen(process.env.PORT, () => {
+  console.log(`✨ Server is running on port ${process.env.PORT}. ✨`);
+});
